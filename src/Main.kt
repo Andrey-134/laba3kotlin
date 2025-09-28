8a67dda3bad176edb7993ec63816fb2fd37f1525
Андрей-134 <aasin2753@gmail.com>
2025-09-28 15:51:52 +0300

    Задание 1. Угадай число 1

--- src/Main.kt
+++ src/Main.kt
@@ -143,5 +143,25 @@ fun main(){
 //val phrase = "${part1 [rand1]} ${part2 [rand2]} ${part3 [rand3]}"
 //println("Baш титул: $phrase")
     // ДЗ 3
+//fun main() {
+//    //Task 1
+//    val sNumber = Random.nextInt(1, 51)
+//    var guess: Int
+//    var at = 0
+//
+//    println("Угадайте число от 1 до 50:")
+//    while (true) {
+//        guess = readln().toInt()
+//        at++
+//
+//        when {
+//            guess < sNumber -> println("Больше!")
+//            guess > sNumber -> println("Меньше!")
+//            else -> {
+//                println("Поздравляю! Вы угадали число $sNumber за $at попыток")
+//                break
+//            }
+//        }
+//    }
 
 }
